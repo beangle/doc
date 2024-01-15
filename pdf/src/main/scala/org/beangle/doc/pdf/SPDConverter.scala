ce0579c97593aa967646ae0eb0acf7460592c8e3
段体华 <duantihua@gmail.com>
2024-01-15 20:14:48 +0800

    fix doc convert error

--- pdf/src/main/scala/org/beangle/doc/pdf/SPDConverter.scala
+++ pdf/src/main/scala/org/beangle/doc/pdf/SPDConverter.scala
@@ -79,7 +79,7 @@ class SPDConverter(pdfMaker: PdfMaker) extends Logging {
     }
     if (result && options.orientation == Orientation.Landscape) {
       val portrait = new File(pdf.getParent + File.separator + Strings.replace(pdf.getName, ".pdf", ".portrait.pdf"))
-      Rotator.rotate(portrait, pdf, -90)
+      Rotator.rotate(pdf, portrait, -90)
       pdf.delete()
       portrait.renameTo(pdf)
     }
